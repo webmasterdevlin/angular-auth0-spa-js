948e00a8e30bd7b325a5813d1359c79acabba36e
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-07-16 21:40:28 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { RouterModule } from "@angular/router";
+import { CallbackComponent } from "./callback/callback.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { AuthGuard } from "./auth.guard";
+import { CollectionComponent } from "./collection/collection.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CallbackComponent,
+    ProfileComponent,
+    CollectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "callback",
+        component: CallbackComponent
+      },
+      {
+        path: "profile",
+        component: ProfileComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: "collection",
+        component: CollectionComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: "**",
+        redirectTo: ""
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
